f3a4ac40bc21c55c9bb7664d8ab86053eb243aad
joshwebpoll <joshharyomide@gmail.com>
2025-07-27 11:56:37 +0100

    fixed some issues

--- controllers/userAuthController.js
+++ controllers/userAuthController.js
@@ -2,12 +2,12 @@ const StatusCodes = require("http-status-codes");
 const User = require("../models/userModel");
 const bcryptjs = require("bcryptjs");
 const crypto = require("crypto");
-const {
-  sendEmailToNewUsers,
-  sendMailVerificationSuccess,
-  sendForgotEmailLink,
-  sendResetSuccessEmail,
-} = require("../mailtrap/sendEmailsToUser");
+// const {
+//   sendEmailToNewUsers,
+//   sendMailVerificationSuccess,
+//   sendForgotEmailLink,
+//   sendResetSuccessEmail,
+// } = require("../mailtrap/sendEmailsToUser");
 const generateJwtCookiesToken = require("../libs/generateJwtTokenCookies");
 // const uploadImage = async (req, res) => {
 //   const result = req.file;
